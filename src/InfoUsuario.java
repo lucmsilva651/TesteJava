8927af5067415c47cce957c8fffd9e089bfd22c4
Luquinhas <lucmsilva651@gmail.com>
2025-08-14 12:47:40 -0300

    Adicionado suporte a Maven

--- src/InfoUsuario.java
+++ src/InfoUsuario.java
@@ -1,5 +1,5 @@
 public class InfoUsuario {
     public static String gerarString(int i, String n, double a, String c) {
-        return String.format("Nome: %s\nIdade: %d\nAltura: %1s\nCidade: %s", n, i, a, c);
+        return String.format("Nome: %s\nIdade: %d\nAltura: %1sm\nCidade: %s", n, i, a, c);
     }
 }
